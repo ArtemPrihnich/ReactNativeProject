9eb3ef50344e12e35c62cc42ffbd99a97c1b2177
Artem Prihnich <artem.prihnich@gmail.com>
2023-03-10 14:17:38 +0200

    package versions changed to work with ReactNativeDebugger

--- App.js
+++ App.js
@@ -6,22 +6,22 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Dimensions,
-} from "react-native";
-import RegistrationScreen from "./Screens/RegistrationScreen";
-import { useCallback } from "react";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import { KeyboardAvoidingView } from "react-native";
-import LoginScreen from "./Screens/LoginScreen";
+} from 'react-native';
+import RegistrationScreen from './Screens/RegistrationScreen';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { KeyboardAvoidingView } from 'react-native';
+import LoginScreen from './Screens/LoginScreen';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const height = Dimensions.get('window').height
+  const height = Dimensions.get('window').height;
   const [fontsLoaded] = useFonts({
-    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
-    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
   });
 
   const onLayoutRootView = useCallback(async () => {
@@ -36,26 +36,23 @@ export default function App() {
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.container} onLayout={onLayoutRootView}>
-        <ImageBackground
-          style={styles.image}
-          source={require("./assets/images/bgImage.jpg")}
-        >
+        <ImageBackground style={styles.image} source={require('./assets/images/bgImage.jpg')}>
           <RegistrationScreen />
           {/* <LoginScreen /> */}
         </ImageBackground>
       </View>
-     </TouchableWithoutFeedback>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#ffffff",
+    backgroundColor: '#ffffff',
   },
   image: {
     flex: 1,
-    resizeMode: "cover",
-    justifyContent: "flex-end",
+    resizeMode: 'cover',
+    justifyContent: 'flex-end',
   },
 });
