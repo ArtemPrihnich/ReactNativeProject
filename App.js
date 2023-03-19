28f7c7a13e19e3fb44de377617f3132bca3e178d
Artem Prihnich <artem.prihnich@gmail.com>
2023-03-19 16:34:22 +0200

    added Auth and MainScreens navigations

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
 // import { StatusBar } from 'expo-status-bar';
-import {
-  StyleSheet,
-  ImageBackground,
-  View,
-  TouchableWithoutFeedback,
-  Keyboard,
-  Dimensions,
-} from 'react-native';
-import RegistrationScreen from './Screens/RegistrationScreen';
+import { StyleSheet, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { useCallback } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import { KeyboardAvoidingView } from 'react-native';
-import LoginScreen from './Screens/LoginScreen';
+import { useRoute } from './utils/router';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const height = Dimensions.get('window').height;
+  const routing = useRoute(false);
   const [fontsLoaded] = useFonts({
     'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
     'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
@@ -36,11 +28,7 @@ export default function App() {
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.container} onLayout={onLayoutRootView}>
-        <ImageBackground style={styles.image} source={require('./assets/images/bgImage.jpg')}>
-          <RegistrationScreen />
-          {/* <NewRegistartionScreen /> */}
-          {/* <LoginScreen /> */}
-        </ImageBackground>
+        <NavigationContainer>{routing}</NavigationContainer>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -50,10 +38,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
-  },
-  image: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'flex-end',
   },
 });
